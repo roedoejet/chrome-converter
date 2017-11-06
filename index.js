5b3fd6072bfa737981237309cbf55cd076b3cd39
John Nenniger <JPNENNIGER@GMAIL.com>
2017-11-06 14:37:24 -0800

    added a sample icon to get it working, will change later

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// This file affects the DOM of the newly created page
+
 $('document').ready(function(){
   // This function will receive the highlighted text from background.js
   chrome.runtime.onMessage.addListener(function(request, response, sendResponse) {
