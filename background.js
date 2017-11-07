0cef6223d096c5b7c44d42fa8e738414f250b7b4
John Nenniger <JPNENNIGER@GMAIL.com>
2017-11-07 14:51:13 -0800

    background js working as indented. Now I need to get the text to populate the newly created tab

--- background.js
+++ background.js
@@ -1,19 +1,18 @@
-
 // this is the event listener for the button on the chrome browser
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({}, function(newtab){
     // Check to see if I'm accessing the right tab
-    console.log(tab.id)
+    console.log(tab.id);
     // send a message to the content script (getHighlighited.js) that gets highlighted text
     chrome.tabs.sendMessage(tab.id, {todo: "Give me something"}, function(highlighted){
-      return sendhighlighted({highlighted: highlighted})
-          .then((response) => {
-            console.log(response)
-            chrome.tabs.sendMessage(newtab.id, {response, highlighted}, function(reply){
-            })
-        })
+      chrome.tabs.sendMessage(newtab.id, {response, highlighted}, function(reply){
+      console.log(reply);
+      })
 
     })
 
-  }
-}
+  })
+  // this should make the event listener act asynchronously so the new tab
+  // doesn't break because of events happening in the wrong order
+  return true
+})
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // This file affects the DOM of the newly created page
-
 $('document').ready(function(){
   // This function will receive the highlighted text from background.js
   chrome.runtime.onMessage.addListener(function(request, response, sendResponse) {
