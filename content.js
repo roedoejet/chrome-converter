87085e5b45bd6ed1cca666a856a00b85a7903ed1
John Nenniger <JPNENNIGER@GMAIL.com>
2018-03-07 17:42:17 -0800

    trying to use convertText correctly

--- content.js
+++ content.js
@@ -54,10 +54,9 @@ let elements = getElements(false);
 // the content within that node that is highlighted
 
 if (elements.length === 1){
-  let toBeReinserted = subtractString(elements[0].textContent, window.getSelection().toString())
-  const parent = elements[0].parentNode
-  parent.replaceChild()
-
+  let highlightedText = window.getSelection().toString();
+  console.log(convertText)
+  replacePartialNodeText(elements[0], highlightedText, convertText)
   elements = elements.slice(1)
 // if nothing is highlighted, we want to translate the whole page, so nothing
 // needs to go here
@@ -91,13 +90,10 @@ function subtractString(main, sub){
   }
 }
 
-function replacePartialNodeText(){
-
-}
 
 var cors = { "Heiltsuk Duolos": [{ "to": "7", "from": "a" }, { "to": "kv", "from": "kv" }, { "to": "k\u0313v", "from": "\u00f0v" }, { "to": "xv", "from": "xv" }, { "to": "g\u030cv", "from": "\u00a9v" }, { "to": "qv", "from": "qv" }, { "to": "q\u0313v", "from": "\u0153v" }, { "to": "x\u030cv", "from": "\u00fev" }, { "to": "x\u030c", "from": "\u00ffx" }, { "to": "X\u030c", "from": "\u00ffX" }, { "to": "b", "from": "b" }, { "to": "p", "from": "p" }, { "to": "p\u0313", "from": "\u00b9" }, { "to": "m", "from": "m" }, { "to": "\u1e43\u0301", "from": "\u00a1" }, { "to": "\u1e43", "from": "\u2122" }, { "to": "m\u0313", "from": "\u00b5" }, { "to": "\u1e43\u0313", "from": "\u00b2" }, { "to": "d", "from": "d" }, { "to": "t\u0313", "from": "\u2020" }, { "to": "n", "from": "n" }, { "to": "\u1e47\u0301", "from": "\u00a2" }, { "to": "\u1e47", "from": "\u00de" }, { "to": "n\u0313", "from": "\u00f1" }, { "to": "\u1e47\u0313", "from": "\u00a6" }, { "to": "z", "from": "z" }, { "to": "c", "from": "c" }, { "to": "c\u0313", "from": "\u00e7" }, { "to": "s", "from": "s" }, { "to": "\u03bb", "from": "\u00df" }, { "to": "\u019b", "from": "\u00d2" }, { "to": "\u019b\u0313", "from": "\u0192" }, { "to": "\u026b", "from": "\u00e6" }, { "to": "\u2c62", "from": "\u00c6" }, { "to": "l", "from": "l" }, { "to": "\u1e37\u0301", "from": "\u00f8" }, { "to": "\u1e37", "from": "\u00aa" }, { "to": "l\u0313", "from": "\u00ac" }, { "to": "\u1e37\u0313", "from": "\u2026" }, { "to": "g", "from": "g" }, { "to": "k", "from": "k" }, { "to": "k\u0313", "from": "\u00f0" }, { "to": "x", "from": "x" }, { "to": "y", "from": "y" }, { "to": "i\u0301", "from": "\u00ed" }, { "to": "y\u0313", "from": "\u00a5" }, { "to": "i\u0313", "from": "\u00ee" }, { "to": "w", "from": "w" }, { "to": "u\u0301", "from": "\u00fa" }, { "to": "u", "from": "u" }, { "to": "w\u0313", "from": "\u201e" }, { "to": "u\u0313", "from": "\u00fc" }, { "to": "g\u030c", "from": "\u00a9" }, { "to": "q", "from": "q" }, { "to": "q\u0313", "from": "\u0153" }, { "to": "x\u030c", "from": "\u00fe" }, { "to": "h", "from": "h" }, { "to": "a\u0301", "from": "\u00e1" }, { "to": "a", "from": "a" }, { "to": "h\u0313", "from": "\u00d3" }, { "to": "a\u0313", "from": "\u00e5" }, { "to": "\u0127", "from": "\u00d4" }, { "to": "\u0294", "from": "\u2030" }, { "to": "\u2144", "from": "\u00cd" }, { "to": "\u2144", "from": "\u00ce" }, { "to": "\u2144\u0313", "from": "\u00cf" }], "Heiltsuk Times": [{ "to": "g", "from": "gv" }, { "to": "k", "from": "kv" }, { "to": "gv", "from": "gv" }, { "to": "kv", "from": "kv" }, { "to": "k\u0313v", "from": "\u02dav" }, { "to": "xv", "from": "xv" }, { "to": "g\u030cv", "from": "\u00a9v" }, { "to": "qv", "from": "qv" }, { "to": "q\u0313v", "from": "\u0153v" }, { "to": "x\u030cv", "from": "\u2248v" }, { "to": "b", "from": "b" }, { "to": "p", "from": "p" }, { "to": "p\u0313", "from": "\u03c0" }, { "to": "m", "from": "m" }, { "to": "\u1e43\u0301", "from": "\u00a1" }, { "to": "\u1e43", "from": "\u2122" }, { "to": "m\u0313", "from": "\u00b5" }, { "to": "\u1e43\u0313", "from": "\u2264" }, { "to": "d", "from": "d" }, { "to": "t\u0313", "from": "\u2020" }, { "to": "n", "from": "n" }, { "to": "\u1e47\u0301", "from": "\u00a2" }, { "to": "\u1e47", "from": "\u221e" }, { "to": "n\u0313", "from": "\u00f1" }, { "to": "\u1e47\u0313", "from": "\u222b" }, { "to": "z", "from": "z" }, { "to": "c", "from": "c" }, { "to": "c\u0313", "from": "\u00e7" }, { "to": "s", "from": "s" }, { "to": "\u03bb", "from": "\u00df" }, { "to": "\u019b", "from": "\u2202" }, { "to": "\u019b\u0313", "from": "\u0192" }, { "to": "\u026b", "from": "\u00e6" }, { "to": "l", "from": "l" }, { "to": "\u1e37\u0301", "from": "\u00f8" }, { "to": "\u1e37", "from": "\u00aa" }, { "to": "l\u0313", "from": "\u00ac" }, { "to": "\u1e37\u0313", "from": "\u2026" }, { "to": "k\u0313", "from": "\u02da" }, { "to": "x", "from": "x" }, { "to": "y", "from": "y" }, { "to": "i\u0301", "from": "\u00ed" }, { "to": "y\u0313", "from": "\u00a5" }, { "to": "i\u0313", "from": "\u00ee" }, { "to": "w", "from": "w" }, { "to": "u\u0301", "from": "\u00fa" }, { "to": "u", "from": "u" }, { "to": "w\u0313", "from": "\u2211" }, { "to": "u\u0313", "from": "\u00fc" }, { "to": "g\u030c", "from": "\u00a9" }, { "to": "q", "from": "q" }, { "to": "q\u0313", "from": "\u0153" }, { "to": "x\u030c", "from": "\u2248" }, { "to": "h", "from": "h" }, { "to": "a\u0301", "from": "\u00e1" }, { "to": "a", "from": "a" }, { "to": "h\u0313", "from": "\u02d9" }, { "to": "a\u0313", "from": "\u00e5" }, { "to": "x\u030c", "from": "\u2248" }, { "to": "\u2144", "from": "\u00cd" }, { "to": "\u2144", "from": "\u00ce" }, { "to": "\u2144\u0313", "from": "\u00cf" }] }
 
-var convertText = function(text, input, output) {
+function convertText(text, input, output) {
     if (text !== undefined && input !== undefined && output !== undefined) {
         if (output !== "Unicode") {
             var text_to_convert = text;
@@ -119,6 +115,16 @@ var convertText = function(text, input, output) {
     }
 };
 
+function replacePartialNodeText(node, selectedText, func){
+  let totalNodeText = node.textContent
+  let subtracted = subtractString(totalNodeText, selectedText);
+  let subtractedTotalNodeText = subtracted[0];
+  let positionToReinsert = subtracted[1];
+  let translated = func(selectedText, "Heiltsuk Duolos", "Unicode");
+  let translatedTotalNodeText = [subtractedTotalNodeText.slice(0, positionToReinsert), translated, subtractedTotalNodeText.slice(positionToReinsert)].join("");
+  let parentOfSelected = node.parentNode
+  parentOfSelected.replaceChild(document.createTextNode(translatedTotalNodeText), node);
+}
 
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
