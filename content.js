feaaad5df69274188e52af5929f242b87f1cd4b2
John Nenniger <JPNENNIGER@GMAIL.com>
2018-03-13 15:22:08 -0700

    works when highlighted text is within a single node

--- content.js
+++ content.js
@@ -3,6 +3,34 @@
 // node in the range, because this function just gets the whole node rather than
 // only the chunk that is needed.
 
+
+var cors = { "Heiltsuk Duolos": [{ "to": "7", "from": "a" }, { "to": "kv", "from": "kv" }, { "to": "k\u0313v", "from": "\u00f0v" }, { "to": "xv", "from": "xv" }, { "to": "g\u030cv", "from": "\u00a9v" }, { "to": "qv", "from": "qv" }, { "to": "q\u0313v", "from": "\u0153v" }, { "to": "x\u030cv", "from": "\u00fev" }, { "to": "x\u030c", "from": "\u00ffx" }, { "to": "X\u030c", "from": "\u00ffX" }, { "to": "b", "from": "b" }, { "to": "p", "from": "p" }, { "to": "p\u0313", "from": "\u00b9" }, { "to": "m", "from": "m" }, { "to": "\u1e43\u0301", "from": "\u00a1" }, { "to": "\u1e43", "from": "\u2122" }, { "to": "m\u0313", "from": "\u00b5" }, { "to": "\u1e43\u0313", "from": "\u00b2" }, { "to": "d", "from": "d" }, { "to": "t\u0313", "from": "\u2020" }, { "to": "n", "from": "n" }, { "to": "\u1e47\u0301", "from": "\u00a2" }, { "to": "\u1e47", "from": "\u00de" }, { "to": "n\u0313", "from": "\u00f1" }, { "to": "\u1e47\u0313", "from": "\u00a6" }, { "to": "z", "from": "z" }, { "to": "c", "from": "c" }, { "to": "c\u0313", "from": "\u00e7" }, { "to": "s", "from": "s" }, { "to": "\u03bb", "from": "\u00df" }, { "to": "\u019b", "from": "\u00d2" }, { "to": "\u019b\u0313", "from": "\u0192" }, { "to": "\u026b", "from": "\u00e6" }, { "to": "\u2c62", "from": "\u00c6" }, { "to": "l", "from": "l" }, { "to": "\u1e37\u0301", "from": "\u00f8" }, { "to": "\u1e37", "from": "\u00aa" }, { "to": "l\u0313", "from": "\u00ac" }, { "to": "\u1e37\u0313", "from": "\u2026" }, { "to": "g", "from": "g" }, { "to": "k", "from": "k" }, { "to": "k\u0313", "from": "\u00f0" }, { "to": "x", "from": "x" }, { "to": "y", "from": "y" }, { "to": "i\u0301", "from": "\u00ed" }, { "to": "y\u0313", "from": "\u00a5" }, { "to": "i\u0313", "from": "\u00ee" }, { "to": "w", "from": "w" }, { "to": "u\u0301", "from": "\u00fa" }, { "to": "u", "from": "u" }, { "to": "w\u0313", "from": "\u201e" }, { "to": "u\u0313", "from": "\u00fc" }, { "to": "g\u030c", "from": "\u00a9" }, { "to": "q", "from": "q" }, { "to": "q\u0313", "from": "\u0153" }, { "to": "x\u030c", "from": "\u00fe" }, { "to": "h", "from": "h" }, { "to": "a\u0301", "from": "\u00e1" }, { "to": "a", "from": "a" }, { "to": "h\u0313", "from": "\u00d3" }, { "to": "a\u0313", "from": "\u00e5" }, { "to": "\u0127", "from": "\u00d4" }, { "to": "\u0294", "from": "\u2030" }, { "to": "\u2144", "from": "\u00cd" }, { "to": "\u2144", "from": "\u00ce" }, { "to": "\u2144\u0313", "from": "\u00cf" }], "Heiltsuk Times": [{ "to": "g", "from": "gv" }, { "to": "k", "from": "kv" }, { "to": "gv", "from": "gv" }, { "to": "kv", "from": "kv" }, { "to": "k\u0313v", "from": "\u02dav" }, { "to": "xv", "from": "xv" }, { "to": "g\u030cv", "from": "\u00a9v" }, { "to": "qv", "from": "qv" }, { "to": "q\u0313v", "from": "\u0153v" }, { "to": "x\u030cv", "from": "\u2248v" }, { "to": "b", "from": "b" }, { "to": "p", "from": "p" }, { "to": "p\u0313", "from": "\u03c0" }, { "to": "m", "from": "m" }, { "to": "\u1e43\u0301", "from": "\u00a1" }, { "to": "\u1e43", "from": "\u2122" }, { "to": "m\u0313", "from": "\u00b5" }, { "to": "\u1e43\u0313", "from": "\u2264" }, { "to": "d", "from": "d" }, { "to": "t\u0313", "from": "\u2020" }, { "to": "n", "from": "n" }, { "to": "\u1e47\u0301", "from": "\u00a2" }, { "to": "\u1e47", "from": "\u221e" }, { "to": "n\u0313", "from": "\u00f1" }, { "to": "\u1e47\u0313", "from": "\u222b" }, { "to": "z", "from": "z" }, { "to": "c", "from": "c" }, { "to": "c\u0313", "from": "\u00e7" }, { "to": "s", "from": "s" }, { "to": "\u03bb", "from": "\u00df" }, { "to": "\u019b", "from": "\u2202" }, { "to": "\u019b\u0313", "from": "\u0192" }, { "to": "\u026b", "from": "\u00e6" }, { "to": "l", "from": "l" }, { "to": "\u1e37\u0301", "from": "\u00f8" }, { "to": "\u1e37", "from": "\u00aa" }, { "to": "l\u0313", "from": "\u00ac" }, { "to": "\u1e37\u0313", "from": "\u2026" }, { "to": "k\u0313", "from": "\u02da" }, { "to": "x", "from": "x" }, { "to": "y", "from": "y" }, { "to": "i\u0301", "from": "\u00ed" }, { "to": "y\u0313", "from": "\u00a5" }, { "to": "i\u0313", "from": "\u00ee" }, { "to": "w", "from": "w" }, { "to": "u\u0301", "from": "\u00fa" }, { "to": "u", "from": "u" }, { "to": "w\u0313", "from": "\u2211" }, { "to": "u\u0313", "from": "\u00fc" }, { "to": "g\u030c", "from": "\u00a9" }, { "to": "q", "from": "q" }, { "to": "q\u0313", "from": "\u0153" }, { "to": "x\u030c", "from": "\u2248" }, { "to": "h", "from": "h" }, { "to": "a\u0301", "from": "\u00e1" }, { "to": "a", "from": "a" }, { "to": "h\u0313", "from": "\u02d9" }, { "to": "a\u0313", "from": "\u00e5" }, { "to": "x\u030c", "from": "\u2248" }, { "to": "\u2144", "from": "\u00cd" }, { "to": "\u2144", "from": "\u00ce" }, { "to": "\u2144\u0313", "from": "\u00cf" }] }
+
+
+
+
+convertText = function(text, input, output) {
+    if (text !== undefined && input !== undefined && output !== undefined) {
+        if (output !== "Unicode") {
+            var text_to_convert = text;
+            var number_of_changes = cors[output].length
+            for (var i = 0; i < number_of_changes; i++) {
+                var re = new RegExp(cors[output][i]["to"], 'g')
+                text_to_convert = text_to_convert.replace(re, cors[output][i]["from"]);
+            }
+            return text_to_convert
+        } else if (output === "Unicode") {
+            var text_to_convert = text;
+            var number_of_changes = cors[input].length
+            for (var i = 0; i < number_of_changes; i++) {
+                var re = new RegExp(cors[input][i]["from"], 'g')
+                text_to_convert = text_to_convert.replace(re, cors[input][i]["to"]);
+            }
+            return text_to_convert
+        }
+    }
+};
+
 // The solution is to remove the first and last node in the HTMLCollection, and
 // write a function which only gets the highlighted text within those two nodes.
 // All other nodes will be passed to the two loops.
@@ -48,16 +76,22 @@ function getElements(isStart) {
     }
 }
 
-//var elements = getElements(false);
-console.log(elements)
+var elements = getElements(false);
+console.log(elements, elements[0].children)
 // if there the highlighted text only spans one node, we need to only replace
 // the content within that node that is highlighted
-
-if (elements.length === 1){
-  let highlightedText = window.getSelection().toString();
-  console.log(convertText)
-  replacePartialNodeText(elements[0], highlightedText, convertText)
-  elements = elements.slice(1)
+var selection = window.getSelection()
+if (selection.anchorNode === selection.focusNode){
+  let highlighted = selection.toString();
+  // replacePartialNodeText(elements[0], highlightedText, convertText)
+  let totalText = selection.anchorNode.textContent
+  let subtracted = subtractString(totalText, highlighted)
+  let translated = convertText(highlighted, "Heiltsuk Duolos", "Unicode")
+  let translatedTotalNodeText = [subtracted[0].slice(0, subtracted[1]), translated, subtracted[0].slice(subtracted[1])].join("");
+  let selNode = selection.anchorNode
+  let parentOfSelected = selNode.parentNode
+  parentOfSelected.replaceChild(document.createTextNode(translatedTotalNodeText), selNode);
+  elements = [];
 // if nothing is highlighted, we want to translate the whole page, so nothing
 // needs to go here
 } else if (window.getSelection().toString() === ""){
@@ -89,31 +123,6 @@ function subtractString(main, sub){
     return [result, pos]
   }
 }
-
-
-var cors = { "Heiltsuk Duolos": [{ "to": "7", "from": "a" }, { "to": "kv", "from": "kv" }, { "to": "k\u0313v", "from": "\u00f0v" }, { "to": "xv", "from": "xv" }, { "to": "g\u030cv", "from": "\u00a9v" }, { "to": "qv", "from": "qv" }, { "to": "q\u0313v", "from": "\u0153v" }, { "to": "x\u030cv", "from": "\u00fev" }, { "to": "x\u030c", "from": "\u00ffx" }, { "to": "X\u030c", "from": "\u00ffX" }, { "to": "b", "from": "b" }, { "to": "p", "from": "p" }, { "to": "p\u0313", "from": "\u00b9" }, { "to": "m", "from": "m" }, { "to": "\u1e43\u0301", "from": "\u00a1" }, { "to": "\u1e43", "from": "\u2122" }, { "to": "m\u0313", "from": "\u00b5" }, { "to": "\u1e43\u0313", "from": "\u00b2" }, { "to": "d", "from": "d" }, { "to": "t\u0313", "from": "\u2020" }, { "to": "n", "from": "n" }, { "to": "\u1e47\u0301", "from": "\u00a2" }, { "to": "\u1e47", "from": "\u00de" }, { "to": "n\u0313", "from": "\u00f1" }, { "to": "\u1e47\u0313", "from": "\u00a6" }, { "to": "z", "from": "z" }, { "to": "c", "from": "c" }, { "to": "c\u0313", "from": "\u00e7" }, { "to": "s", "from": "s" }, { "to": "\u03bb", "from": "\u00df" }, { "to": "\u019b", "from": "\u00d2" }, { "to": "\u019b\u0313", "from": "\u0192" }, { "to": "\u026b", "from": "\u00e6" }, { "to": "\u2c62", "from": "\u00c6" }, { "to": "l", "from": "l" }, { "to": "\u1e37\u0301", "from": "\u00f8" }, { "to": "\u1e37", "from": "\u00aa" }, { "to": "l\u0313", "from": "\u00ac" }, { "to": "\u1e37\u0313", "from": "\u2026" }, { "to": "g", "from": "g" }, { "to": "k", "from": "k" }, { "to": "k\u0313", "from": "\u00f0" }, { "to": "x", "from": "x" }, { "to": "y", "from": "y" }, { "to": "i\u0301", "from": "\u00ed" }, { "to": "y\u0313", "from": "\u00a5" }, { "to": "i\u0313", "from": "\u00ee" }, { "to": "w", "from": "w" }, { "to": "u\u0301", "from": "\u00fa" }, { "to": "u", "from": "u" }, { "to": "w\u0313", "from": "\u201e" }, { "to": "u\u0313", "from": "\u00fc" }, { "to": "g\u030c", "from": "\u00a9" }, { "to": "q", "from": "q" }, { "to": "q\u0313", "from": "\u0153" }, { "to": "x\u030c", "from": "\u00fe" }, { "to": "h", "from": "h" }, { "to": "a\u0301", "from": "\u00e1" }, { "to": "a", "from": "a" }, { "to": "h\u0313", "from": "\u00d3" }, { "to": "a\u0313", "from": "\u00e5" }, { "to": "\u0127", "from": "\u00d4" }, { "to": "\u0294", "from": "\u2030" }, { "to": "\u2144", "from": "\u00cd" }, { "to": "\u2144", "from": "\u00ce" }, { "to": "\u2144\u0313", "from": "\u00cf" }], "Heiltsuk Times": [{ "to": "g", "from": "gv" }, { "to": "k", "from": "kv" }, { "to": "gv", "from": "gv" }, { "to": "kv", "from": "kv" }, { "to": "k\u0313v", "from": "\u02dav" }, { "to": "xv", "from": "xv" }, { "to": "g\u030cv", "from": "\u00a9v" }, { "to": "qv", "from": "qv" }, { "to": "q\u0313v", "from": "\u0153v" }, { "to": "x\u030cv", "from": "\u2248v" }, { "to": "b", "from": "b" }, { "to": "p", "from": "p" }, { "to": "p\u0313", "from": "\u03c0" }, { "to": "m", "from": "m" }, { "to": "\u1e43\u0301", "from": "\u00a1" }, { "to": "\u1e43", "from": "\u2122" }, { "to": "m\u0313", "from": "\u00b5" }, { "to": "\u1e43\u0313", "from": "\u2264" }, { "to": "d", "from": "d" }, { "to": "t\u0313", "from": "\u2020" }, { "to": "n", "from": "n" }, { "to": "\u1e47\u0301", "from": "\u00a2" }, { "to": "\u1e47", "from": "\u221e" }, { "to": "n\u0313", "from": "\u00f1" }, { "to": "\u1e47\u0313", "from": "\u222b" }, { "to": "z", "from": "z" }, { "to": "c", "from": "c" }, { "to": "c\u0313", "from": "\u00e7" }, { "to": "s", "from": "s" }, { "to": "\u03bb", "from": "\u00df" }, { "to": "\u019b", "from": "\u2202" }, { "to": "\u019b\u0313", "from": "\u0192" }, { "to": "\u026b", "from": "\u00e6" }, { "to": "l", "from": "l" }, { "to": "\u1e37\u0301", "from": "\u00f8" }, { "to": "\u1e37", "from": "\u00aa" }, { "to": "l\u0313", "from": "\u00ac" }, { "to": "\u1e37\u0313", "from": "\u2026" }, { "to": "k\u0313", "from": "\u02da" }, { "to": "x", "from": "x" }, { "to": "y", "from": "y" }, { "to": "i\u0301", "from": "\u00ed" }, { "to": "y\u0313", "from": "\u00a5" }, { "to": "i\u0313", "from": "\u00ee" }, { "to": "w", "from": "w" }, { "to": "u\u0301", "from": "\u00fa" }, { "to": "u", "from": "u" }, { "to": "w\u0313", "from": "\u2211" }, { "to": "u\u0313", "from": "\u00fc" }, { "to": "g\u030c", "from": "\u00a9" }, { "to": "q", "from": "q" }, { "to": "q\u0313", "from": "\u0153" }, { "to": "x\u030c", "from": "\u2248" }, { "to": "h", "from": "h" }, { "to": "a\u0301", "from": "\u00e1" }, { "to": "a", "from": "a" }, { "to": "h\u0313", "from": "\u02d9" }, { "to": "a\u0313", "from": "\u00e5" }, { "to": "x\u030c", "from": "\u2248" }, { "to": "\u2144", "from": "\u00cd" }, { "to": "\u2144", "from": "\u00ce" }, { "to": "\u2144\u0313", "from": "\u00cf" }] }
-
-function convertText(text, input, output) {
-    if (text !== undefined && input !== undefined && output !== undefined) {
-        if (output !== "Unicode") {
-            var text_to_convert = text;
-            var number_of_changes = cors[output].length
-            for (var i = 0; i < number_of_changes; i++) {
-                var re = new RegExp(cors[output][i]["to"], 'g')
-                text_to_convert = text_to_convert.replace(re, cors[output][i]["from"]);
-            }
-            return text_to_convert
-        } else if (output === "Unicode") {
-            var text_to_convert = text;
-            var number_of_changes = cors[input].length
-            for (var i = 0; i < number_of_changes; i++) {
-                var re = new RegExp(cors[input][i]["from"], 'g')
-                text_to_convert = text_to_convert.replace(re, cors[input][i]["to"]);
-            }
-            return text_to_convert
-        }
-    }
-};
 
 function replacePartialNodeText(node, selectedText, func){
   let totalNodeText = node.textContent
